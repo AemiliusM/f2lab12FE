9342addf31c4ad20cec74f582cc4a1e22afea83e
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-24 13:37:29 -0700

    basic setup and added authjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
+import Auth from './Auth.js';
+class Home extends Component {
+  state = {  }
+  render() { 
+    return ( <h1>Home Page</h1> );
+  }
 }
+ 
+export default Home;
 
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <BrowserRouter>
+      <header>
+        <NavLink to='/' exact>
+          Home
+        </NavLink>
+        <NavLink to='/signin'>Sign In</NavLink>
+        <NavLink to='/signup'>Sign Up</NavLink>
+      </header>
+      <section className='main'></section>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/signin'>
+          <Auth type='signin'/>
+        </Route>
+        <Route path='/signup'>
+          <Auth type='signup'/>
+        </Route>
+      </Switch>
+      </BrowserRouter>
+     );
+  }
+}
+ 
 export default App;
